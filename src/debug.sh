7c036d97cca1d11b8515070db02519818a26d4fd
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2023-01-13 22:29:22 -0500

    debug.sh: clean up

--- src/debug.sh
+++ src/debug.sh
@@ -1,49 +1,52 @@
 #!/usr/bin/env bash
 
 # @file Debug
-# @brief Functions to facilitate debugging scripts.
+# @brief Functions to facilitate debugging.
 
-# @description Prints the content of array as key value pair for easier debugging.
-# Pass the variable name of the array instead of value of the variable.
+# @description Print the contents of an array as key-value pairs.
+# Pass the name of the array variable instead of its value.
+#
 # @example
-#   array=(foo bar baz)
-#   printf "Array\n"
-#   printarr "array"
-#   declare -A assoc_array
-#   assoc_array=([foo]=bar [baz]=foobar)
-#   printf "Assoc Array\n"
-#   printarr "assoc_array"
+#   arr=(foo bar baz)
+#   printf "Array:\n"
+#   debug::print_array "arr"
 #   #Output
-#   Array
+#   Array:
 #   0 = foo
 #   1 = bar
 #   2 = baz
-#   Assoc Array
-#   baz = foobar
-#   foo = bar
-#
-# @arg $1 string variable name of the array.
-#
-# @stdout Formatted key value of array.
-debug::print_array() {
-    declare -n __arr="$1"
-    for k in "${!__arr[@]}"; do printf "%s = %s\n" "$k" "${__arr[$k]}"; done
-}
-
-# @description Function to print ansi escape sequence as is.
-# This function helps debug ansi escape sequence in text by displaying the escape codes.
 #
 # @example
-#   txt="$(tput bold)$(tput setaf 9)This is bold red text$(tput sgr0).$(tput setaf 10)This is green text$(tput sgr0)"
+#   declare -A arr2=([foo]=bar [baz]=qux)
+#   printf "Associative array:\n"
+#   debug::print_array "arr2"
+#   #Output
+#   Associative Array:
+#   foo = bar
+#   baz = qux
+#
+# @arg $1 string Name of the array variable.
+#
+# @stdout Formatted key-value pairs of the array.
+debug::print_array() {
+    local -n ref="$1"
+    for i in "${!ref[@]}"; do
+        printf "%s = %s\n" "$i" "${ref[$i]}"
+    done
+}
+
+# @description Print ANSI escape sequences as-is.
+# This function helps debug ANSI escape sequences in text by displaying the escape codes as-is.
+#
+# @example
+#   txt="$(tput bold)$(tput setaf 9)This is bold red text$(tput sgr0). $(tput setaf 10)This is green text$(tput sgr0)."
 #   debug::print_ansi "$txt"
 #   #Output
-#   \e[1m\e[91mThis is bold red text\e(B\e[m.\e[92mThis is green text\e(B\e[m
+#   \e[1m\e[91mThis is bold red text\e(B\e[m. \e[92mThis is green text\e(B\e[m.
 #
-# @arg $1 string input with ansi escape sequence.
+# @arg $1 string Input string with optional ANSI escape sequence(s).
 #
-# @stdout Ansi escape sequence printed in output as is.
+# @stdout Input string with ANSI escape sequence(s) printed as-is.
 debug::print_ansi() {
-    #echo $(tr -dc '[:print:]'<<<$1)
-    printf "%s\n" "${1//$'\e'/\\e}"
-
+    printf "%s" "${1//$'\e'/\\e}"
 }
